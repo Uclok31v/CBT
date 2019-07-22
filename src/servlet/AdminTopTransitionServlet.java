be8dc79c2ba754df94ddd9dc6a574a0c34f4a8bc
鵜戸西謙太郎 <ei.1px.9.01@gmail.com>
2019-07-23 00:39:00 +0900

    add AdminTopTransition

---
+++ src/servlet/AdminTopTransitionServlet.java
@@ -0,0 +1,52 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import beans.UserBean;
+import dao.UserDao;
+
+/**
+ * Servlet implementation class AdminTopTransitionServlet
+ */
+
+public class AdminTopTransitionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdminTopTransitionServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		
+		ArrayList<UserBean> beginnerList = new UserDao().selectUserByTimes();
+		ArrayList<UserBean> failureList = new UserDao().selectUserByResult();
+		request.setAttribute("beginner-list", beginnerList);
+		request.setAttribute("failure-list", failureList);
+		request.getRequestDispatcher("/jsp/admin/adminTop.jsp").forward(request, response);
+	}
+
+}
