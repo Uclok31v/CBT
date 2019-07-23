588963c00fe821195946076bc6bc4582a766cc25
鵜戸西謙太郎 <ei.1px.9.01@gmail.com>
2019-07-23 21:13:56 +0900

    Add QuestionSearch

---
+++ src/servlet/QuestionSearchServlet.java
@@ -0,0 +1,52 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import beans.QuestionBean;
+import dao.QuestionDao;
+
+/**
+ * Servlet implementation class QuestionSearchServlet
+ */
+
+public class QuestionSearchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public QuestionSearchServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		
+		String keyword = request.getParameter("keyword");
+		
+		ArrayList<QuestionBean> questionList = new QuestionDao().selectQuestionByText(keyword);
+		request.setAttribute("question-list", questionList);
+		request.getRequestDispatcher("/jsp/admin/questionList.jsp").forward(request, response);
+	}
+
+}
